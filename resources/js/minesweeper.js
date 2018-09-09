39042b9f0312bf588574f650eb31d2d1b71850a6
David Baker <daviiddamiianbaker@gmail.com>
2018-09-09 16:52:08 -0400

    wip hard coding game board

--- resources/js/minesweeper.js
+++ resources/js/minesweeper.js
@@ -1,1 +1,14 @@
-let board = [];
+const printBoard = board => {
+    console.log(`Current Board: `);
+
+};
+
+let board = [
+[' ', ' ', ' '], 
+[' ', ' ', ' '], 
+[' ', ' ', ' ']
+];
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
