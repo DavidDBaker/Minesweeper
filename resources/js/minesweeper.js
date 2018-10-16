89bed6d33f6a656461d076e7b6a7a64060b1ef36
David Baker <daviiddamiianbaker@gmail.com>
2018-10-16 01:47:38 -0400

    creating functions to generate game board

--- resources/js/minesweeper.js
+++ resources/js/minesweeper.js
@@ -1,14 +1,40 @@
-const printBoard = board => {
-    console.log(`Current Board: `);
-
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const gameBoard = [];
+    for(let rowIndex=0; rowIndex<numberOfRows; rowIndex++){
+        const row = [];
+        gameBoard.push(row);
+        for (let columnIndex=0; columnIndex<numberOfColumns; columnIndex++){
+           row.push(' '); 
+        }
+    }
+    return gameBoard;
 };
 
-let board = [
-[' ', ' ', ' '], 
-[' ', ' ', ' '], 
-[' ', ' ', ' ']
-];
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const gameBoard = [];
+    for(let rowIndex=0; rowIndex<numberOfRows; rowIndex++){
+        const row = [];
+        gameBoard.push(row);
+        for (let columnIndex=0; columnIndex<numberOfColumns; columnIndex++){
+           row.push(null); 
+        }
+    }
+    let numberOfBombsPlaced = 0;
+
+    while(numberOfBombsPlaced < numberOfBombs){
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);     
+        gameBoard[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+    return gameBoard;
+};
+
+const printBoard = gameBoard => {
+    gameBoard.map(row => row.join(' | ')).join('\n');
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+printBoard(playerBoard);
+printBoard(bombBoard);
